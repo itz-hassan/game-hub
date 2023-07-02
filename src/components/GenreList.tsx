23fe626867c7e8d28d88d3469aa2e7add9353f68
Hassan Usman <husmanjah@gmail.com>
2023-07-02 15:16:20 +0100

    showing a spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,11 @@
-import { Genre } from "../hooks/useGenres";
-import useData from "../hooks/useData";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import useGenre from "../hooks/useGenres";
+import { HStack, Image, List, ListItem, Text, Spinner } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useData<Genre>("/genres");
+  const { data, isLoading, error } = useGenre();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {data.map((genre) => (
